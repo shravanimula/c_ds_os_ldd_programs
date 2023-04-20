103b1e6522607d1d346dd012fcc208763b4bd44b
shravani <shravani.mula@thundersoft.com>
2023-04-20 11:46:15 +0530

    remov

--- c_test/own_sizeof.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#define mysize(type) (char *)(&type + 1) - (char *)(&type)
-int main()
-{
-	char i;
-	printf("size of integer is :%ld\n",mysize(i));
-}
--- c_test/volatile.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	volatile int i=10;
-	int a=100;
-	printf("%d\t%d\t%d\t%d\n",++a,--a,++a,--a);
-	printf("%d\t%d\t%d\t%d\n",++i,--i,++i,--i);
-}
