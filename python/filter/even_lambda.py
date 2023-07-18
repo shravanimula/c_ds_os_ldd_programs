b3075f866992ba4d903b0592a321ad5f306d6daf
shravanimula <98573934+shravanimula@users.noreply.github.com>
2023-07-18 17:55:30 +0530

    python_programs

---
+++ python/filter/even_lambda.py
@@ -0,0 +1,8 @@
+L=[2,3,4,6,7,8,9,10]
+print("the even numbers are:")
+L1=list(filter(lambda x:x%2==0,L))
+print(L1)
+l=[2,3,4,5,6,7,8,9,10]
+print("the odd numbers are:")
+l1=list(filter(lambda y:y%2==1,l))
+print(l1)  
