a1b0c6921cf5e4b9978e5ed4d2b20589fcff656e
shravanimula <98573934+shravanimula@users.noreply.github.com>
2023-08-21 18:51:21 +0530

    format_function

---
+++ PYTHON_Programs/format_fun.py
@@ -0,0 +1,9 @@
+s="my name is {fname},I have {exp} experience".format(fname="aaaaa",exp=2)
+print(s)
+
+s1="my name is {0},I have {1} experience".format("aaa",1)
+
+print(s1)
+
+s2="iam in {},my name is {}".format("hyd","bbb")
+print(s2)
